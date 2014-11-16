8beb1bae54a04759c8b24f56d3cc5286abf1102a
sanjaysubramanian <sanjaysubramanian@yahoo.com>
2014-11-16 09:18:10 -0800

    new spark version in POM

--- src/main/scala/org/medicalsidefx/common/utils/HelloWorld.scala
+++ src/main/scala/org/medicalsidefx/common/utils/HelloWorld.scala
@@ -1,7 +1,19 @@
 package org.medicalsidefx.common.utils
 
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 object HelloWorld {
     def main(args: Array[String]) {
       println("Hello, Cruel world!")
+      val conf = new SparkConf()
+      			     .setMaster("local2")
+      			     .setAppName("foofla")
+      			     .set("spark.executor.memory","1g")
+      			     .set("spark.rdd.compress", "true")
+      			     .set("spark.storage.memoryFraction", "1")
+      val sc = new SparkContext(conf)
+      val data = sc.parallelize(1 to 1000000).collect().filter(_<1000)
+      data.foreach(println)
     }
   }
