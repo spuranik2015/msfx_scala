19c463606eca29961b50ad54ed1925808e31916b
Sanjay Subramanian <sanjaysubramanian@yahoo.com>
2014-11-19 14:36:25 -0800

    added TwoWayJoin

---
+++ src/main/scala/org/medicalsidefx/common/utils/TwoWayJoin.scala
@@ -0,0 +1,29 @@
+package org.medicalsidefx.common.utils
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+/**
+ * Created by sansub01 on 11/19/14.
+ */
+object TwoWayJoin {
+  def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.println("Usage: TwoWayJoinCount <file1>   <file2>")
+      System.exit(12)
+    }
+
+    val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-TwoWayJoin")
+
+    val sc = new SparkContext(sconf)
+
+    val file1 = args(0)
+    val file2 = args(1)
+
+    val file1Rdd = sc.textFile(file1).map(x => (x.split(",")(0), x.split(",")(1)))
+    val file2Rdd = sc.textFile(file2).map(x => (x.split(",")(0), x.split(",")(1)))
+
+    file1Rdd.join(file2Rdd).foreach(println)
+  }
+}
