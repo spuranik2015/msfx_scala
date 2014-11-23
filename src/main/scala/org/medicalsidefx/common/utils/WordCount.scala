aad43a445498d46b7ff25abb7757bd25cb1acee8
sanjaysubramanian <sanjaysubramanian@yahoo.com>
2014-11-23 09:27:22 -0800

    added saveAstextFile

--- src/main/scala/org/medicalsidefx/common/utils/WordCount.scala
+++ src/main/scala/org/medicalsidefx/common/utils/WordCount.scala
@@ -13,12 +13,19 @@ object WordCount {
 
     val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-ScalaAnalytics")
     val sc = new SparkContext(sconf)
-    val file = args(0)
+    val inputDir = args(0)
+    val outDir = args(1)
+    /*
     val counts = sc.textFile(file).
       flatMap(line => line.split("\\W")).
       map(word => (word,1)).
       reduceByKey((v1,v2) => v1+v2).foreach(println)
+      */
 
+    sc.textFile(inputDir).
+      flatMap(line => line.split("\\W")).
+      map(word => (word,1)).
+      reduceByKey((v1,v2) => v1+v2).saveAsTextFile(outDir+"/out_"+System.currentTimeMillis)
 //    counts.take(10).foreach(println)
   }
 }
