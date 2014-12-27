3e029d3422810e063d9e30010221fecd3829624a
sanjaysubramanian <sanjaysubramanian@yahoo.com>
2014-12-27 15:04:17 -0800

    changed list to map

--- src/main/scala/org/medicalsidefx/common/utils/AersDemoColumnExtractor.scala
+++ src/main/scala/org/medicalsidefx/common/utils/AersDemoColumnExtractor.scala
@@ -6,53 +6,57 @@ import org.apache.spark.{SparkConf, SparkContext}
  * Created by sanjay on 12/23/14.
  */
 object AersDemoColumnExtractor {
+  def main(args: Array[String]) {
+    val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-AersDemoColumnExtractor")
+    val sc = new SparkContext(sconf)
 
-  val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-AersDemoColumnExtractor")
-  val sc = new SparkContext(sconf)
+    val demohdrV1 = List("/data/aers/quarterly_files/aers_ascii_1999q1/ascii/demo/DEMO99Q1.TXT", "/data/aers/quarterly_files/aers_ascii_1999q2/ascii/demo/DEMO99Q2.TXT", "/data/aers/quarterly_files/aers_ascii_1999q3/ascii/demo/DEMO99Q3.TXT", "/data/aers/quarterly_files/aers_ascii_1999q4/ascii/demo/DEMO99Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2000q1/ascii/demo/demo00q1.txt", "/data/aers/quarterly_files/aers_ascii_2000q2/ascii/demo/demo00q2.txt", "/data/aers/quarterly_files/aers_ascii_2000q3/ascii/demo/demo00q3.txt", "/data/aers/quarterly_files/aers_ascii_2000q4/ascii/demo/demo00q4.txt", "/data/aers/quarterly_files/aers_ascii_2001q1/ascii/demo/demo01q1.txt", "/data/aers/quarterly_files/aers_ascii_2001q2/ascii/demo/demo01q2.txt", "/data/aers/quarterly_files/aers_ascii_2001q3/ascii/demo/demo01q3.txt", "/data/aers/quarterly_files/aers_ascii_2001q4/ascii/demo/demo01q4.txt", "/data/aers/quarterly_files/aers_ascii_2002q1/ascii/demo/demo02q1.txt")
+    val demohdrV2 = List("/data/aers/quarterly_files/aers_ascii_2002q2/ascii/demo/demo02q2.txt")
+    val demohdrV3 = List("/data/aers/quarterly_files/aers_ascii_2002q3/ascii/demo/demo02q3.txt", "/data/aers/quarterly_files/aers_ascii_2002q4/ascii/demo/demo02q4.txt", "/data/aers/quarterly_files/aers_ascii_2003q1/ascii/demo/DEMO03Q1.TXT", "/data/aers/quarterly_files/aers_ascii_2003q2/ascii/demo/DEMO03Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2003q3/ascii/demo/DEMO03Q3.TXT", "/data/aers/quarterly_files/aers_ascii_2003q4/ascii/demo/DEMO03Q4.TXT", "/data/aers/quarterly_files/AERS_ASCII_2004q1/ascii/demo/DEMO04Q1.TXT", "/data/aers/quarterly_files/AERS_ASCII_2004q2/ascii/demo/DEMO04Q2.TXT", "/data/aers/quarterly_files/AERS_ASCII_2004q3/ascii/demo/DEMO04Q3.TXT", "/data/aers/quarterly_files/AERS_ASCII_2004q4/ascii/demo/DEMO04Q4.TXT", "/data/aers/quarterly_files/AERS_ASCII_2005Q1/ascii/demo/DEMO05Q1.TXT", "/data/aers/quarterly_files/AERS_ASCII_2005Q2/ascii/demo/DEMO05Q2.TXT")
+    val demohdrV4 = List("/data/aers/quarterly_files/AERS_ASCII_2005Q3/ascii/demo/DEMO05Q3.TXT", "/data/aers/quarterly_files/AERS_ASCII_2005Q4/ascii/demo/DEMO05Q4.TXT", "/data/aers/quarterly_files/AERS_ASCII_2006Q1/ascii/demo/DEMO06Q1.TXT", "/data/aers/quarterly_files/AERS_ASCII_2006Q2/ascii/demo/DEMO06Q2.TXT", "/data/aers/quarterly_files/AERS_ASCII_2006Q3/ascii/demo/DEMO06Q3.TXT", "/data/aers/quarterly_files/AERS_ASCII_2006Q4/ascii/demo/DEMO06Q4.TXT", "/data/aers/quarterly_files/AERS_ASCII_2007Q1/ascii/demo/DEMO07Q1.TXT", "/data/aers/quarterly_files/AERS_ASCII_2007Q2/ascii/demo/DEMO07Q2.TXT", "/data/aers/quarterly_files/AERS_ASCII_2007Q3/ascii/demo/DEMO07Q3.TXT", "/data/aers/quarterly_files/AERS_ASCII_2007Q4/ascii/demo/DEMO07Q4.TXT", "/data/aers/quarterly_files/AERS_ASCII_2008Q1/ascii/demo/DEMO08Q1.TXT", "/data/aers/quarterly_files/AERS_ASCII_2008Q2/ascii/demo/DEMO08Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2008q3/ascii/demo/DEMO08Q3.TXT", "/data/aers/quarterly_files/aers_ascii_2008q4/ascii/demo/DEMO08Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2009q1/ascii/demo/DEMO09Q1.TXT", "/data/aers/quarterly_files/aers_ascii_2009q2/ascii/demo/DEMO09Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2009q3/ascii/demo/DEMO09Q3.TXT", "/data/aers/quarterly_files/aers_ascii_2009q4/ascii/demo/DEMO09Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2010q1/ascii/demo/DEMO10Q1.TXT", "/data/aers/quarterly_files/aers_ascii_2010q2/ascii/demo/DEMO10Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2010q3/ascii/demo/DEMO10Q3.TXT", "/data/aers/quarterly_files/aers_ascii_2010q4/ascii/demo/DEMO10Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2011q1/ascii/demo/DEMO11Q1.TXT", "/data/aers/quarterly_files/aers_ascii_2011q2/ascii/demo/DEMO11Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2011q3/ascii/demo/DEMO11Q3.TXT", "/data/aers/quarterly_files/aers_ascii_2011q4/ascii/demo/DEMO11Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2012q1/ascii/demo/DEMO12Q1.TXT", "/data/aers/quarterly_files/aers_ascii_2012q2/ascii/demo/DEMO12Q2.TXT", "/data/aers/quarterly_files/aers_ascii_2012q3/ascii/demo/DEMO12Q3.TXT")
+    val demohdrV5 = List("/data/aers/quarterly_files/AERS_ASCII_2013q3/ascii/demo/DEMO13Q3.txt", "/data/aers/quarterly_files/faers_ascii_2012q4/ascii/demo/demo12q4.txt", "/data/aers/quarterly_files/faers_ascii_2013q1/ascii/demo/DEMO13Q1.txt", "/data/aers/quarterly_files/faers_ascii_2013q2/ascii/demo/DEMO13Q2.txt", "/data/aers/quarterly_files/FAERS_ASCII_2013Q4/ascii/demo/DEMO13Q4.txt", "/data/aers/quarterly_files/faers_ascii_2014q1/ascii/demo/DEMO14Q1.txt")
 
-  val demohdrV1 =  List("/data/aers/quarterly_files/aers_ascii_1999q1/ascii/demo/DEMO99Q1.TXT", "/data/aers/quarterly_files/aers_ascii_1999q2/ascii/demo/DEMO99Q2.TXT", "/data/aers/quarterly_files/aers_ascii_1999q3/ascii/demo/DEMO99Q3.TXT", "/data/aers/quarterly_files/aers_ascii_1999q4/ascii/demo/DEMO99Q4.TXT", "/data/aers/quarterly_files/aers_ascii_2000q1/ascii/demo/demo00q1.txt", "/data/aers/quarterly_files/aers_ascii_2000q2/ascii/demo/demo00q2.txt", "/data/aers/quarterly_files/aers_ascii_2000q3/ascii/demo/demo00q3.txt", "/data/aers/quarterly_files/aers_ascii_2000q4/ascii/demo/demo00q4.txt", "/data/aers/quarterly_files/aers_ascii_2001q1/ascii/demo/demo01q1.txt", "/data/aers/quarterly_files/aers_ascii_2001q2/ascii/demo/demo01q2.txt", "/data/aers/quarterly_files/aers_ascii_2001q3/ascii/demo/demo01q3.txt", "/data/aers/quarterly_files/aers_ascii_2001q4/ascii/demo/demo01q4.txt", "/data/aers/quarterly_files/aers_ascii_2002q1/ascii/demo/demo02q1.txt")
-  val demohdrV2 =  List("/data/aers/quarterly_files/aers_ascii_2002q2/ascii/demo/demo02q2.txt")
-  val demohdrV3 = List("/data/aers/quarterly_files/aers_ascii_2002q3/ascii/demo/demo02q3.txt","/data/aers/quarterly_files/aers_ascii_2002q4/ascii/demo/demo02q4.txt","/data/aers/quarterly_files/aers_ascii_2003q1/ascii/demo/DEMO03Q1.TXT","/data/aers/quarterly_files/aers_ascii_2003q2/ascii/demo/DEMO03Q2.TXT","/data/aers/quarterly_files/aers_ascii_2003q3/ascii/demo/DEMO03Q3.TXT","/data/aers/quarterly_files/aers_ascii_2003q4/ascii/demo/DEMO03Q4.TXT","/data/aers/quarterly_files/AERS_ASCII_2004q1/ascii/demo/DEMO04Q1.TXT","/data/aers/quarterly_files/AERS_ASCII_2004q2/ascii/demo/DEMO04Q2.TXT","/data/aers/quarterly_files/AERS_ASCII_2004q3/ascii/demo/DEMO04Q3.TXT","/data/aers/quarterly_files/AERS_ASCII_2004q4/ascii/demo/DEMO04Q4.TXT","/data/aers/quarterly_files/AERS_ASCII_2005Q1/ascii/demo/DEMO05Q1.TXT","/data/aers/quarterly_files/AERS_ASCII_2005Q2/ascii/demo/DEMO05Q2.TXT")
-  val demohdrV4 = List("/data/aers/quarterly_files/AERS_ASCII_2005Q3/ascii/demo/DEMO05Q3.TXT","/data/aers/quarterly_files/AERS_ASCII_2005Q4/ascii/demo/DEMO05Q4.TXT","/data/aers/quarterly_files/AERS_ASCII_2006Q1/ascii/demo/DEMO06Q1.TXT","/data/aers/quarterly_files/AERS_ASCII_2006Q2/ascii/demo/DEMO06Q2.TXT","/data/aers/quarterly_files/AERS_ASCII_2006Q3/ascii/demo/DEMO06Q3.TXT","/data/aers/quarterly_files/AERS_ASCII_2006Q4/ascii/demo/DEMO06Q4.TXT","/data/aers/quarterly_files/AERS_ASCII_2007Q1/ascii/demo/DEMO07Q1.TXT","/data/aers/quarterly_files/AERS_ASCII_2007Q2/ascii/demo/DEMO07Q2.TXT","/data/aers/quarterly_files/AERS_ASCII_2007Q3/ascii/demo/DEMO07Q3.TXT","/data/aers/quarterly_files/AERS_ASCII_2007Q4/ascii/demo/DEMO07Q4.TXT","/data/aers/quarterly_files/AERS_ASCII_2008Q1/ascii/demo/DEMO08Q1.TXT","/data/aers/quarterly_files/AERS_ASCII_2008Q2/ascii/demo/DEMO08Q2.TXT","/data/aers/quarterly_files/aers_ascii_2008q3/ascii/demo/DEMO08Q3.TXT","/data/aers/quarterly_files/aers_ascii_2008q4/ascii/demo/DEMO08Q4.TXT","/data/aers/quarterly_files/aers_ascii_2009q1/ascii/demo/DEMO09Q1.TXT","/data/aers/quarterly_files/aers_ascii_2009q2/ascii/demo/DEMO09Q2.TXT","/data/aers/quarterly_files/aers_ascii_2009q3/ascii/demo/DEMO09Q3.TXT","/data/aers/quarterly_files/aers_ascii_2009q4/ascii/demo/DEMO09Q4.TXT","/data/aers/quarterly_files/aers_ascii_2010q1/ascii/demo/DEMO10Q1.TXT","/data/aers/quarterly_files/aers_ascii_2010q2/ascii/demo/DEMO10Q2.TXT","/data/aers/quarterly_files/aers_ascii_2010q3/ascii/demo/DEMO10Q3.TXT","/data/aers/quarterly_files/aers_ascii_2010q4/ascii/demo/DEMO10Q4.TXT","/data/aers/quarterly_files/aers_ascii_2011q1/ascii/demo/DEMO11Q1.TXT","/data/aers/quarterly_files/aers_ascii_2011q2/ascii/demo/DEMO11Q2.TXT","/data/aers/quarterly_files/aers_ascii_2011q3/ascii/demo/DEMO11Q3.TXT","/data/aers/quarterly_files/aers_ascii_2011q4/ascii/demo/DEMO11Q4.TXT","/data/aers/quarterly_files/aers_ascii_2012q1/ascii/demo/DEMO12Q1.TXT","/data/aers/quarterly_files/aers_ascii_2012q2/ascii/demo/DEMO12Q2.TXT","/data/aers/quarterly_files/aers_ascii_2012q3/ascii/demo/DEMO12Q3.TXT")
-  val demohdrV5 = List("/data/aers/quarterly_files/AERS_ASCII_2013q3/ascii/demo/DEMO13Q3.txt","/data/aers/quarterly_files/faers_ascii_2012q4/ascii/demo/demo12q4.txt","/data/aers/quarterly_files/faers_ascii_2013q1/ascii/demo/DEMO13Q1.txt","/data/aers/quarterly_files/faers_ascii_2013q2/ascii/demo/DEMO13Q2.txt","/data/aers/quarterly_files/FAERS_ASCII_2013Q4/ascii/demo/DEMO13Q4.txt","/data/aers/quarterly_files/faers_ascii_2014q1/ascii/demo/DEMO14Q1.txt")
+//    val demoList = List(demohdrV1, demohdrV2, demohdrV3, demohdrV4, demohdrV5)
+    val demoList = Map("demohdrV1" -> demohdrV1,
+                        "demohdrV2" -> demohdrV2,
+                        "demohdrV3" -> demohdrV3,
+                        "demohdrV4" -> demohdrV4,
+                        "demohdrV5" -> demohdrV5)
 
-  val demoList = List(demohdrV1,demohdrV2,demohdrV3,demohdrV4,demohdrV5)
-
-  var outFile = ""
-
-  for (b <- demoList) {
-    for (a <- b) {
-      var demoRdd = sc.textFile(a)
-      try {
-        outFile = a.reverse.split('/')(0).split('.')(1).reverse
-      }
-      catch {
-        case e: Exception => println(a.toString + " - Exception");
-      }
-      if (outFile != null) {
-        try {
-          var demoRddMap = demoRdd.map(line => {
-            var tokens = line.split('$')
-            if (tokens.length >= 13 && !line.contains("ISR$CASE$I_F_COD$FOLL_SEQ") && !line.contains("primaryid$caseid$caseversion")){
-              var foofla = a.toString()
-              if (a.toString().contains("demoHeaderVersion5")){
-                tokens(0) + "\t" + tokens(4) + "\t" + tokens(11) + "\t" + tokens(12)
+    var outFile = ""
+      for ((key,value) <- demoList) {
+          for (a <- value) {
+            var demoRdd = sc.textFile(a)
+            try {
+              outFile = a.reverse.split('/')(0).split('.')(1).reverse
+            }
+            catch {
+              case e: Exception => println(a.toString + " - Exception");
+            }
+            if (outFile != null) {
+              try {
+                var demoRddMap = demoRdd.map(line => {
+                  var tokens = line.split('$')
+                  if (tokens.length >= 13 && !line.contains("ISR$CASE$I_F_COD$FOLL_SEQ") && !line.contains("primaryid$caseid$caseversion")) {
+                    if (key.toString().contains("demohdrV5")) {
+                      tokens(0) + "\t" + tokens(4) + "\t" + tokens(11) + "\t" + tokens(12)
+                    }
+                    else {
+                      tokens(0) + "\t" + tokens(5) + "\t" + tokens(11) + "\t" + tokens(12)
+                    }
+                  }
+                  else {
+                    ""
+                  }
+                })
+                demoRddMap.saveAsTextFile("/data/aers/msfx/demo/" + outFile)
+                println("SUCCESSFUL " + a.toString)
               }
-              else {
-                tokens(0) + "\t" + tokens(5) + "\t" + tokens(11) + "\t" + tokens(12)
+              catch {
+                case e: Exception => println("FAILED " + a.toString)
               }
             }
-            else {
-                 ""
-            }
-          })
-          demoRddMap.saveAsTextFile("/data/aers/msfx/demo/" + outFile)
-          println("SUCCESSFUL "+a.toString)
-        }
-        catch {
-          case e: Exception => println("FAILED "+a.toString)
-        }
+          }
       }
-    }
   }
 }
--- src/main/scala/org/medicalsidefx/common/utils/AersDrugColumnExtractor.scala
+++ src/main/scala/org/medicalsidefx/common/utils/AersDrugColumnExtractor.scala
@@ -6,44 +6,46 @@ import org.apache.spark.{SparkConf, SparkContext}
  * Created by sanjay on 12/23/14.
  */
 object AersDrugColumnExtractor {
+  def main(args: Array[String]) {
 
-  val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-AersDrugColumnExtractor")
-  val sc = new SparkContext(sconf)
+    val sconf = new SparkConf().setMaster("local").setAppName("MedicalSideFx-AersDrugColumnExtractor")
+    val sc = new SparkContext(sconf)
 
-  val drughdrV1 =  List("\"/data/aers/quarterly_files/aers_ascii_1999q1/ascii/drug/DRUG99Q1.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q2/ascii/drug/DRUG99Q2.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q3/ascii/drug/DRUG99Q3.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q4/ascii/drug/DRUG99Q4.TXT\",\"/data/aers/quarterly_files/aers_ascii_2000q1/ascii/drug/drug00q1.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q2/ascii/drug/drug00q2.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q3/ascii/drug/drug00q3.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q4/ascii/drug/drug00q4.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q1/ascii/drug/drug01q1.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q2/ascii/drug/drug01q2.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q3/ascii/drug/drug01q3.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q4/ascii/drug/drug01q4.txt\",\"/data/aers/quarterly_files/aers_ascii_2002q1/ascii/drug/drug02q1.txt\"")
-  val drughdrV2 =  List("")
-  val drughdrV3 = List("")
-  val drughdrV4 = List("")
+    val drughdrV1 = List("\"/data/aers/quarterly_files/aers_ascii_1999q1/ascii/drug/DRUG99Q1.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q2/ascii/drug/DRUG99Q2.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q3/ascii/drug/DRUG99Q3.TXT\",\"/data/aers/quarterly_files/aers_ascii_1999q4/ascii/drug/DRUG99Q4.TXT\",\"/data/aers/quarterly_files/aers_ascii_2000q1/ascii/drug/drug00q1.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q2/ascii/drug/drug00q2.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q3/ascii/drug/drug00q3.txt\",\"/data/aers/quarterly_files/aers_ascii_2000q4/ascii/drug/drug00q4.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q1/ascii/drug/drug01q1.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q2/ascii/drug/drug01q2.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q3/ascii/drug/drug01q3.txt\",\"/data/aers/quarterly_files/aers_ascii_2001q4/ascii/drug/drug01q4.txt\",\"/data/aers/quarterly_files/aers_ascii_2002q1/ascii/drug/drug02q1.txt\"")
+    val drughdrV2 = List("")
+    val drughdrV3 = List("")
+    val drughdrV4 = List("")
 
-  val demoList = List(drughdrV1,drughdrV2,drughdrV3,drughdrV4)
+    val demoList = List(drughdrV1, drughdrV2, drughdrV3, drughdrV4)
 
-  var outFile = ""
+    var outFile = ""
 
-  for (b <- demoList) {
-    for (a <- b) {
-      var demoRdd = sc.textFile(a)
-      try {
-        outFile = a.reverse.split('/')(0).split('.')(1).reverse
-      }
-      catch {
-        case e: Exception => println(a.toString + " - Exception");
-      }
-      if (outFile != null) {
+    for (b <- demoList) {
+      for (a <- b) {
+        var demoRdd = sc.textFile(a)
         try {
-          var demoRddMap = demoRdd.map(line => {
-            var tokens = line.split('$')
-            if (tokens.length >= 13 && !line.contains("ISR$CASE$I_F_COD$FOLL_SEQ") && !line.contains("primaryid$caseid$caseversion")){
-              tokens(0) + "\t" + tokens(5) + "\t" + tokens(11) + "\t" + tokens(12)
-            }
-            else {
-                 ""
-            }
-          })
-          demoRddMap.saveAsTextFile("/data/aers/msfx/demo/" + outFile)
-          println("SUCCESSFUL "+a.toString)
+          outFile = a.reverse.split('/')(0).split('.')(1).reverse
         }
         catch {
-          case e: Exception => println("FAILED "+a.toString)
+          case e: Exception => println(a.toString + " - Exception");
+        }
+        if (outFile != null) {
+          try {
+            var demoRddMap = demoRdd.map(line => {
+              var tokens = line.split('$')
+              if (tokens.length >= 13 && !line.contains("ISR$CASE$I_F_COD$FOLL_SEQ") && !line.contains("primaryid$caseid$caseversion")) {
+                tokens(0) + "\t" + tokens(5) + "\t" + tokens(11) + "\t" + tokens(12)
+              }
+              else {
+                ""
+              }
+            })
+            demoRddMap.saveAsTextFile("/data/aers/msfx/demo/" + outFile)
+            println("SUCCESSFUL " + a.toString)
+          }
+          catch {
+            case e: Exception => println("FAILED " + a.toString)
+          }
         }
       }
     }
